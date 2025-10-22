7b073b816d80a8d708efe80c5aee92ae1e00ffeb
SeungChan <dlatmdcks60@gmail.com>
2025-10-22 14:05:18 +0900

    feat(printWinners): 우승자 판별 및 출력을 위한 printWinners() 구현
    - printWinners() 기본틀 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -57,6 +57,10 @@ class RacingCalculator {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: " + carName);
         }
     }
+
+    private static void printWinners(List<Car> cars) {
+
+    }
 }
 
 class Car {
